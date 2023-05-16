81ddae85b955588eea5441538dbed6de5fd50f6f
Denis Carriere <carriere.denis@gmail.com>
2023-05-16 17:22:35 -0400

    remove extras

--- packages/substreams-proxy/bin/cli.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-
-import "../dist/cjs/cli.js";
--- packages/substreams-react/src/index.ts
+++
@@ -1,8 +0,0 @@
-export {
-  type SubstreamContext,
-  type UseSubstreamStartOptions,
-  type UseSubstreamOptions,
-  useSubstream,
-} from "./useSubstream.js";
-export { useMessageTypeRegistry } from "./useMessageTypeRegistry.js";
-export { useKey } from "./useKey.js";
